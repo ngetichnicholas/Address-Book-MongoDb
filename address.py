93fcb2a0787859327316f871ae0856ed5615254e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-11-19 16:50:25 +0300

    Add mongoDb connection and create db with collection

---
+++ address.py
@@ -0,0 +1,18 @@
+import pymongo
+from pymongo import MongoClient
+from decouple import config
+
+cluster = MongoClient(f"mongodb+srv://NgetichNick:{config('PASS')}@cluster0.0gaxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["address"]
+collection = db["address"]
+post = {
+    {"_id": 1, "name": "Ben", "address": "Berlin 63"},
+    {"_id": 2, "name": "Mark", "address": "Nairobi 46"}
+}
+
+results = collection.find({"name":"Nick"})
+
+for result in results:
+    print(result)
+
+collection.insert_many(post)
